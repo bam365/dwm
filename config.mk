e7e00cdb494fab9a2ceed764851cfcba2f53b0db
Blake Mitchell <bam365@mst.edu>
2014-01-02 06:54:47 -0600

    corrected and added lib and inc paths for freebsd

--- config.mk
+++ config.mk
@@ -1,33 +1,37 @@
 # dwm version
-VERSION = 5.9
+VERSION = 6.0
 
 # Customize below to fit your system
 
 # paths
-PREFIX = /usr
-MANPREFIX = ${PREFIX}/share/man
+PREFIX = /usr/local 
+MANPREFIX = /usr/local
 
-X11INC = /usr/include/X11
-X11LIB = /usr/lib/X11
+X11INC = /usr/local/include
+X11LIB = /usr/local/lib
 
 # Xinerama
 XINERAMALIBS = -L${X11LIB} -lXinerama
 XINERAMAFLAGS = -DXINERAMA
 
+# Xft
+XFTINCS = -I${X11INC}/freetype2
+XFTLIBS = -L${X11LIB} -lXft
+
 # includes and libs
-INCS = -I. -I/usr/include -I${X11INC}
-LIBS = -L/usr/lib -lc -L${X11LIB} -lX11 ${XINERAMALIBS}
+INCS = -I. -I/usr/include -I${X11INC} ${XFTINCS}
+LIBS = -L/usr/lib -lc -L${X11LIB} -lX11 ${XINERAMALIBS} ${XFTLIBS}
 
 # flags
-CPPFLAGS = -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS}
+CPPFLAGS+= -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS}
 #CFLAGS = -g -std=c99 -pedantic -Wall -O0 ${INCS} ${CPPFLAGS}
-CFLAGS = -std=c99 -pedantic -Wall -Os ${INCS} ${CPPFLAGS}
+CFLAGS+= -std=c99 ${INCS} ${CPPFLAGS}
 #LDFLAGS = -g ${LIBS}
-LDFLAGS = -s ${LIBS}
+LDFLAGS+= ${LIBS}
 
 # Solaris
 #CFLAGS = -fast ${INCS} -DVERSION=\"${VERSION}\"
 #LDFLAGS = ${LIBS}
 
 # compiler and linker
-CC = cc
+CC?= cc
